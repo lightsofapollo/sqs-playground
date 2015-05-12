3042fbe36aaa5e872d851bcf11a62807901b1ab4
James Lal <james@lightsofapollo.com>
2015-05-11 21:34:53 -0700

    with sns

--- perf.js
+++ perf.js
@@ -1,23 +1,22 @@
-import { SQS } from 'aws-sdk-promise';
+import { SQS, SNS } from 'aws-sdk-promise';
 import os from 'os';
 import { createHash } from 'crypto';
 
 const host = new Buffer('testing-sqs-' + os.hostname()).toString('base64');
 
 const MESSAGES = 100;
+const TOPIC = 'sqs-to-sns-testing-jlal';
 
-async function sendMessages(sqs, url) {
+async function sendMessages(sns, topicArn) {
   let total = MESSAGES;
   console.time('send');
   while (total--) {
-    await sqs.sendMessage({
-      QueueUrl: url,
-      MessageBody: new Buffer(JSON.stringify({
-        url,
+    let res = await sns.publish({
+      TopicArn: topicArn,
+      Message: new Buffer(JSON.stringify({
         now: Date.now()
       })).toString('base64')
     }).promise();
-
   }
   console.timeEnd('send');
 }
@@ -33,8 +32,8 @@ async function getFromQueue(sqs, url) {
       WaitTimeSeconds: 20
     }).promise();
 
-
     let messages = fetch.data.Messages;
+    if (!messages) continue;
     pendingMessages -= messages.length;
 
     let pendingDeletes = [];
@@ -74,15 +73,70 @@ async function getOrCreateQueue(sqs) {
 
 }
 
+async function createTopic(sns) {
+  let topic = await sns.createTopic({
+    Name: TOPIC,
+  }).promise();
+  return topic.data.TopicArn;
+}
+
 async function main() {
   console.time('create queue');
   let sqs = new SQS({ region: 'us-west-2' });
-  let queueUrl = await getOrCreateQueue(sqs);
+  let sns = new SNS({ region: 'us-west-2' });
 
+  let [topicArn, queueUrl] =
+    await Promise.all([createTopic(sns), getOrCreateQueue(sqs)]);
+
+  let queueDetails = await sqs.getQueueAttributes({
+    QueueUrl: queueUrl,
+    AttributeNames: ['QueueArn']
+  }).promise();
   console.timeEnd('create queue');
 
+  let queueArn = queueDetails.data.Attributes.QueueArn;
+
+  let policy = {
+    Version: '2008-10-17',
+    Id: queueArn + '/SQSDefaultPolicy',
+    Statement: [{
+      Sid: "Sid" + Date.now(),
+      Effect: "Allow",
+      Principal: {
+        // This should be okay since you need specific permission to publish to
+        // the source arn for this to actually allow you to send stuff...
+        AWS: '*'
+      },
+      Action: 'SQS:SendMessage',
+      Resource: queueArn,
+      Condition: {
+        ArnEquals: {
+          'aws:SourceArn': topicArn
+        }
+      }
+    }]
+  }
+
+  await sqs.setQueueAttributes({
+    QueueUrl: queueUrl,
+    Attributes: {
+      Policy: JSON.stringify(policy)
+    }
+  }).promise();
+
+  console.time('subscribe');
+  let params = {
+    TopicArn: topicArn,
+    Protocol: 'sqs',
+    Endpoint: queueArn
+  }
+  console.log(params);
+  let res = await sns.subscribe(params).promise();
+  console.timeEnd('subscribe');
+  console.log(res.data)
+
   await Promise.all([
-    sendMessages(sqs, queueUrl),
+    sendMessages(sns, topicArn),
     getFromQueue(sqs, queueUrl)
   ]);
 }
